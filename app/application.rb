06c93281b76f0bd5ed32c9ef3fec4f1cca064e02
markdegroat <mhdegroat@gmail.com>
2017-04-03 16:11:11 -0400

    Final

---
+++ app/application.rb
@@ -0,0 +1,29 @@
+class Application
+
+  @@items = ["Apples","Carrots","Pears"]
+  @@cart = []
+
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+
+    if req.path.match(/items/)
+      item_name = req.path.split("/items/").last
+      item_to_return = @@items.find do |item|
+        item.name == item_name
+      end
+      if !!item_to_return
+        resp.write item_to_return.price
+      else
+        resp.status = 400
+        resp.write "Item not found"
+      end
+    else
+      resp.status = 404
+      resp.write "Route not found"
+    end
+
+    resp.finish
+  end
+
+end
